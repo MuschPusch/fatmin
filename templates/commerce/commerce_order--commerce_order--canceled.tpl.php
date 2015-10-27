855b1adf45bd9fe62599e361738c6c03aeb8d790
muschpusch <volkan@rhizom.nl>
2015-10-27 10:48:39 +0100

    Add working but ugly credit memo

---
+++ templates/commerce/commerce_order--commerce_order--canceled.tpl.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @file
+ * Template for credit memos (=canceled orders).
+ */
+
+?>
+
+<h1 class="credit-memo" style='color:red; padding:40px;'><?php print t('Credit Memo'); ?></h1>
+
